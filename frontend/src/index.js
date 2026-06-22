36227644e08b14e5a39ac4ec6ddb09be108d7d16
Rachana A <rachanaa5002@gmail.com>
2026-06-22 11:44:47 +0530

    Fix: Remove broken useTheme import to fix frontend build in CI/CD pipeline

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,11 @@ import App from "./App";
 import "@fontsource/roboto";
 import { CssBaseline } from "@mui/material";
 import "./index.css";
-import { ThemeProvider } from "./hooks/useTheme"; // <-- Make sure correct path!
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <CssBaseline />
+    <App />
   </React.StrictMode>
 );
